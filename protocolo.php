f48cc0bf3f10ec374633cae275ace0339a2e370f
afuenzalim <127132543+afuenzalim@users.noreply.github.com>
2024-06-27 00:47:17 -0400

    Add files via upload

---
+++ protocolo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Protocolo PALOMA</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="cabecera">
+		<div class="contenido">
+		<img src="logo.png">
+		<h1>REGISTRO DE PSC POR SECTOR</h1></div>
+	</div>
+
+	<div class="cuerpo">
+	<div class="sidebar">
+		<ul>
+			<li><a href="">¿Qué es protocolo paloma?</a></li>
+		</ul>
+	</div>
+
+	<div class="cuerpo2">
+		<div class="intro">
+			<p>En el sector que usted, como locatario, trabaja y/o reside, es posible ver a personas en situación de calle que pueden necesitar ayuda médica.</p>
+			<p>A través de este formulario usted podra identificar y hacer seguimiento de estas personas para que tanto usted como sus vecinos puedan ayudarlas en un futuro.</p>
+			<p>En el siguiente formulario usted va a encontrar preguntas relacionadas a la identidad de la PSC. El formulario cuenta con 9 preguntas que usted deberá responder mediante texto o marcando casillas, lea atentamente cada pregunta y responda de acuerdo a lo pedido. </p>
+	</div class="formulario">
+		<h2 class="tituloform">LOS DATOS HAN SIDO ENVIADOS CORRECTAMENTE. GRACIAS</h2>
+</div>
+</div>
+
+</body>
+</html>
